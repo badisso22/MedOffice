1862df84a2cd78cd000bcf25ce5bd21506a13e02
badisso <127608561+badisso22@users.noreply.github.com>
2025-12-19 18:36:29 +0100

    updated settings
    added a link to logout from there

--- Patient/settings.php
+++ Patient/settings.php
@@ -354,17 +354,17 @@
             </div>
             <div class="action-row">
               <div class="action-info">
-                <h4>Sign Out Everywhere</h4>
-                <p>Sign out from all devices and browsers</p>
+                <h4>Sign Out</h4>
+                <p>Sign out from your account </p>
               </div>
-              <button class="btn btn-secondary">
+              <a href="../config/logout.php" class="btn btn-secondary">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                   <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                   <polyline points="16 17 21 12 16 7"></polyline>
                   <line x1="21" y1="12" x2="9" y2="12"></line>
                 </svg>
-                Sign Out All
-              </button>
+                Sign Out
+              </a>
             </div>
           </div>
         </div>
