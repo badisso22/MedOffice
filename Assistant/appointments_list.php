5e0a21a4d7f6b47ce43e4460c191bfb32f81bbd4
badisso <127608561+badisso22@users.noreply.github.com>
2025-12-26 01:06:22 +0100

    Update appointments_list.php

--- Assistant/appointments_list.php
+++ Assistant/appointments_list.php
@@ -105,14 +105,6 @@
                 Settings
             </a></li>
         </ul>
-        <button class="drawer-logout" onclick="window.location.href='logout.php'">
-            <svg viewBox="0 0 24 24" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" style="display: inline-block; margin-right: 0.5rem; vertical-align: middle;">
-                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                <polyline points="16 17 21 12 16 7"></polyline>
-                <line x1="21" y1="12" x2="9" y2="12"></line>
-            </svg>
-            Logout
-        </button>
     </div>
 
     <div class="drawer-overlay" id="drawerOverlay" onclick="toggleDrawer()"></div>
