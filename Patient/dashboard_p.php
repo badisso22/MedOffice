55c5801bbe7680f6fa8037666937b12620e0d8bc
badisso <127608561+badisso22@users.noreply.github.com>
2026-01-05 06:59:27 +0100

    Update dashboard_p.php

--- Patient/dashboard_p.php
+++ Patient/dashboard_p.php
@@ -262,14 +262,6 @@ $appointmentsQuery->close();
                 </div>
             </div>
         </section>
-        <!-- Note : this will be removed , and th doctor will automatically send a feedback option via notification to each patient they served-->
-        <a href="feedBack.php" class="feature-card" aria-label="Go to Medical Records">
-                    <div class="feature-icon">
-                        <svg viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line></svg>
-                    </div>
-                    <h3>Feed-back</h3>
-                    <p>give feedback to your consultations</p> 
-        </a>
     </main>
 
     <footer>
